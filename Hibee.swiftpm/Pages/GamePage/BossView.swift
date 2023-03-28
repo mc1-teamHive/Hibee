d111effb1cc4db9c577732c15b63f3bcac893f70
hajime <impapa68@gmail.com>
2023-03-28 15:26:24 +0900

    init with hajime's code

---
+++ Hibee.swiftpm/Pages/GamePage/BossView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Ha Jong Myeong on 2023/03/28.
+//
+import SwiftUI
+
+// set Boss Info
+struct Boss{
+    let names : [String] = ["Issac", "Gommin", "Lingo"]
+    let healths : [Int] = [3, 4, 6]
+}
+
+struct BossView: View {
+    let boss = Boss()
+    var body: some View {
+        Image(uiImage: UIImage(named: "Issac_1.png")!).frame(height: 230)
+    }
+}
+
+struct BossView_Previews: PreviewProvider {
+    static var previews: some View {
+        BossView()
+    }
+}
---
+++ Hibee.swiftpm/Pages/LandingPage/LandingPageView.swift
@@ -0,0 +1,46 @@
+//
+//  LandingPageView.swift
+//  Hibee
+//
+//  Created by Ha Jong Myeong on 2023/03/26.
+//
+import SwiftUI
+
+struct LandingPageView: View {
+    @State private var isCardFlipViewActive = false
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                Spacer()
+                Button("Start") {
+                    // action when press buttons
+                    isCardFlipViewActive = true
+                }
+                .buttonStyle(GrayButton())
+                .padding(.bottom, 150)
+                
+                NavigationLink(destination: GameView(), isActive: $isCardFlipViewActive){
+                    EmptyView()
+                }
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct GrayButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 74/255, green: 92/255, blue: 109/255))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView().previewInterfaceOrientation(.landscapeLeft)
+    }
+}
