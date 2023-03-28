dcaffe248ea28833622169e29488589700025431
Kim, Yerim <yerim343@naver.com>
2023-03-28 21:41:30 +0900

    [yerimee] card animation circle

--- Hibee.swiftpm/Pages/GamePage/CardFlipView.swift
+++ Hibee.swiftpm/Pages/GamePage/CardFlipView.swift
@@ -7,84 +7,44 @@
 
 import SwiftUI
 
-// MARK: Card Front View
-struct CardFront : View {
-    let width : CGFloat
-    let height : CGFloat
-    let num : Int
-    let cardColors: [Color] = [.red, .green, .blue, .yellow, .purple, .orange, .blue, .yellow, .purple, .orange]
-    
-    @Binding var degree : Double
-    
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(cardColors[num % cardColors.count])
-                .frame(width: width, height: height)
-                .shadow(color: .gray, radius: 2, x: 0, y: 0)
-            
-        }.rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
-    }
-}
-
-// MARK: Card Back View
-struct CardBack : View {
-    //MARK: Variables
-    let width : CGFloat
-    let height : CGFloat
-    @Binding var degree : Double
-    
-    var body: some View {
-        ZStack {RoundedRectangle(cornerRadius: 20)
-                .stroke(.gray.opacity(0.7), lineWidth: 3)
-                .frame(width: width, height: height)
-            
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(.clear)
-            Image(uiImage: UIImage(named: "cards.png")!)
-                .frame(width: width, height: height)
-                .shadow(color: .gray, radius: 2, x: 0, y: 0)
-        }.rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
-        
-    }
-}
-
-// MARK: Card Flip View
 struct CardFlipView: View {
-    // MARK: Variables
-    @State var backDegrees = Array(repeating: 0.0, count: 10)
-    @State var frontDegrees = Array(repeating: -90.0, count: 10)
-    @State var isFlipped = Array(repeating: true, count: 10)
-    let width : CGFloat = 200
-    let height : CGFloat = 300
-    let durationAndDelay : CGFloat = 0.3
+    @State private var degrees: [Double] = [-40, -30, -25, -15, -5, 5, 15, 25, 30, 40].map { $0 }
+    @State private var yLocation: [Double] = [100, 100, 80, 60, 50, 50, 60, 80, 100, 120]
+    @State private var xLocation: [Double] = [60, 50, 40, 30, 0, 0, -20, -40, -60, -80]
+    @State private var offsets: [CGSize] = Array(repeating: .zero, count: 10)
+    @State private var isToggle: [Bool] = Array(repeating: false, count: 10)
+    private let cardWidth: CGFloat = 215
+    private let cardHeight: CGFloat = 300
     
-    // MARK: Flip Card Function
-    func flipCard(_ num: Int) {
-        if isFlipped[num] {
-            isFlipped[num] = true
-            withAnimation(.linear(duration: durationAndDelay)){
-                backDegrees[num] = 90
-            }
-            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
-                frontDegrees[num] = 0
-            }
-        }
-    }
+    @State private var isShowingCards = false
     
-    // MARK: View Body
     var body: some View {
-        HStack(spacing: -10) {
-            ForEach(0..<10) { num in
-                        ZStack {
-                            CardFront(width: width, height: height, num: num, degree: $frontDegrees[num])
-                            CardBack(width: width, height: height, degree: $backDegrees[num])
-                        }.onTapGesture {
-                            flipCard(num)
-                        }
-                        .frame(width: 150, height: 400)
-                }
+        HStack(spacing: 0) {
+            ForEach(degrees.indices, id: \.self) { index in
+                CardImage(width: cardWidth, height: cardHeight, degree: $degrees[index], offset: $offsets[index], isToggled: $isToggle[index])
+                    .offset(x: xLocation[index], y: yLocation[index])
+                    .opacity(isShowingCards ? 1 : 0)
+                    .animation(Animation.linear.delay(Double(index) * 0.08), value: isShowingCards)
+            }
+            .padding(.bottom, 10)
         }
-        .navigationBarBackButtonHidden(true)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .padding(.horizontal)
+        .onAppear {
+            for index in offsets.indices {
+                let offset = index - 4
+                offsets[index] = CGSize(width: CGFloat(offset) * -30, height: CGFloat(offset * offset * 10))
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                isShowingCards = true
+            }
+        }
+
+    }
+}
+
+struct CardFlipView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardFlipView()
     }
 }
---
+++ Hibee.swiftpm/Pages/GamePage/CardImage.swift
@@ -0,0 +1,56 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 김예림 on 2023/03/28.
+//
+
+import SwiftUI
+
+struct CardImage: View {
+    let width: CGFloat
+    let height: CGFloat
+    @Binding var degree: Double
+    @Binding var offset: CGSize
+    @Binding var isToggled: Bool
+    
+    var body: some View {
+        ZStack {
+            
+            if !isToggled {
+                Image("Card(B)")
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: width, height: height)
+                
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.white, lineWidth: 10)
+                    .frame(width: width-10, height: height-10)
+                    .shadow(color: Color(red: 0.29, green: 0.36, blue: 0.43).opacity(0.15), radius: 5, x: 0, y: 0)
+            }else{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .frame(width: width, height: height)
+                    .shadow(color: Color(red: 0.29, green: 0.36, blue: 0.43).opacity(0.15), radius: 5, x: 0, y: 0)
+                
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.white, lineWidth: 10)
+                    .frame(width: width-10, height: height-10)
+            }
+        
+        }.rotation3DEffect(Angle(degrees: -degree), axis: (x: 0, y: 0, z: -1))
+            .offset(offset)
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    isToggled = true
+                }
+            }
+    }
+}
+
+
+struct CardImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CardImage(width: 215, height: 300, degree: .constant(0), offset: .constant(.zero), isToggled: .constant(false))
+    }
+}
+
