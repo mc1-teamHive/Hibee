c180e370654a0c61206fa31792185cf80685478b
hajime <impapa68@gmail.com>
2023-03-30 07:07:17 +0900

    fix : navigationBackButton

--- Hibee.swiftpm/Pages/GamePage/GameView.swift
+++ Hibee.swiftpm/Pages/GamePage/GameView.swift
@@ -8,14 +8,14 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-            VStack{
-                // Spacer for dont ignore safearea
-                Spacer().frame(height: 1)
-                TopPanelView()
-                ProgressBar()
-                BossView()
-                CardFlipView()
-            }
+        VStack{
+            // Spacer for dont ignore safearea
+            Spacer().frame(height: 1)
+            TopPanelView()
+            ProgressBar()
+            BossView()
+            CardFlipView()
+        }.navigationBarBackButtonHidden(true)
             .background(Image("backgrounds_1").resizable().scaledToFit().frame(maxWidth: .infinity, maxHeight: .infinity).edgesIgnoringSafeArea(.all))
-        }
+    }
 }
