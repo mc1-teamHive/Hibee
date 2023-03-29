1383e803119d456482fd3e3b9c39a2e1355bb40d
hajime <impapa68@gmail.com>
2023-03-30 04:05:40 +0900

    fix : confilict issue to pull

--- Hibee.swiftpm/Pages/QuizPage/AnswersheetView.swift
+++ Hibee.swiftpm/Pages/QuizPage/AnswersheetView.swift
@@ -24,7 +24,7 @@ struct AnswerSheetView: View {
             Spacer()
             Image(isCorrect ? "Caesar" : "Wrongperson")
                 .resizable()
-            .frame(width: 400, height: 400)}.onTapGesture {
+            .frame(width: 400, height: 400)}.frame(width: 964, height: 864).onTapGesture {
                 isPresented.toggle()
         }
     }
